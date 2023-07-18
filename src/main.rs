42344225efa51d5dbe0ded6809028341aa8571d4
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-18 12:43:05 +0300

    rename

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::OpenOptions;
+use std::io::{Read, stdin, Write};
+
 fn main() {
-    println!("Hello, world!");
+    renamefile();
+}
+
+fn renamefile(){
+    let path = "data.txt";
+
+    std::fs::rename(path,"text.txt").expect("Error!");
+
+}
+
+fn openoptions(){
+    let path = "data.txt";
+
+    let mut f = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path)
+        .expect("Error opening/creating file!");
+    let mut file_data = String::new();
+    f.read_to_string(&mut file_data).expect("Error reading file");
+    println!("File Data:\n{}",file_data);
+
+    println!("Enter something:");
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Error getting user input");
+    f.write_all(input.as_bytes()).expect("Error writing to file!");
 }
